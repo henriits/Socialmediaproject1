ba89f8ebfd905a3e9b3ff6deb4ca136c1a86ae6f
katladva <kat.ladva@hotmail.com>
2023-05-17 22:22:20 +0300

    add new file what_if.py

---
+++ what_if.py
@@ -0,0 +1,8 @@
+day = input('What is the weather like? ')
+
+if day == 'sunny':
+    print('What a lovely day!')
+elif day == 'normal':
+    print('It is ok weather, go out!')
+else:
+    print('Stay inside, no need to go out!')

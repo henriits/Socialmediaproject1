53ff884fa516aff55ef675a41812a2506a50f146
henriits <121551949+henriits@users.noreply.github.com>
2023-05-19 11:27:49 +0300

    created first model for Post

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from sorl.thumbnail import ImageField
+
+
+class Post(models.Model):
+    title = models.CharField(250)
+    text = models.CharField(250)
+    image = ImageField()
+
+    def __str__(self):
+        return self.title

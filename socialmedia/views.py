5c6ddcf4459fb8caf8e800572a46edd0622b759d
henriits <121551949+henriits@users.noreply.github.com>
2023-05-22 15:15:00 +0300

    add userCreateView

--- socialmedia/views.py
+++ socialmedia/views.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.models import User
+
 from posts.models import Post
 from django.urls import reverse_lazy
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 
+from socialmedia.forms import SignUpForm
 
 
 class HomeView(ListView):
@@ -9,3 +12,15 @@ class HomeView(ListView):
     template_name = "home.html"
     success_url = reverse_lazy("home")
     context_object_name = "posts"
+
+
+
+class UserCreateView(CreateView):
+    model = User
+    template_name = 'registration/signup.html'
+    form_class = SignUpForm
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)

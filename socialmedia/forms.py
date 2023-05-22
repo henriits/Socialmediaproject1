f99a0e38ecca922874d2978150dac10647cafb81
henriits <121551949+henriits@users.noreply.github.com>
2023-05-22 15:25:41 +0300

    create SignUpForm

--- socialmedia/forms.py
+++ socialmedia/forms.py
@@ -1,10 +1,10 @@
-from django.contrib.auth import forms
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=254, required=True, help_text='Required. Enter a valid username')
+    email = forms.EmailField(max_length=254, required=True, help_text='Required. Enter a valid email')
 
     class Meta:
         model = User

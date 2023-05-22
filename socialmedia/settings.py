88ede6410d1e26a053526f5e990fab84ba0d5d26
henriits <121551949+henriits@users.noreply.github.com>
2023-05-22 15:44:30 +0300

    added login , logout and signup redirect URLS

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -131,3 +131,10 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = '/posts/'
+LOGOUT_REDIRECT_URL = "/"
+
+SIGNUP_REDIRECT_URL = "/login/"
+

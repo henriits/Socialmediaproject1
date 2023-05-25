7206dbc6cfeee7b4039b55ecd675e591f9ad04d8
katladva <kat.ladva@hotmail.com>
2023-05-25 12:16:40 +0300

    style changes for posts.html

--- posts/models.py
+++ posts/models.py
@@ -6,7 +6,7 @@ from sorl.thumbnail import ImageField
 
 class Post(models.Model):
     title = models.CharField(max_length=250)
-    image = models.ImageField(upload_to="images/")
+    image = models.ImageField(upload_to="images/", blank=True, null=True)
     text = models.CharField(max_length=250)
 
     def __str__(self):
--- socialmedia/urls.py
+++ socialmedia/urls.py
@@ -32,3 +32,5 @@ urlpatterns = [
     # path('profile/', user_views.profile, name='profile'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

72e59d6732e5672c3b1aae89f305d621f13cbca0
katladva <kat.ladva@hotmail.com>
2023-05-26 14:52:05 +0300

    DB fix

--- posts/models.py
+++ posts/models.py
@@ -7,11 +7,11 @@ from sorl.thumbnail import ImageField
 
 
 class Post(models.Model):
-    #author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
     image = models.ImageField(upload_to="images/", blank=True, null=True)
     text = models.CharField(max_length=250)
-    #created_date = models.DateTimeField(default=timezone.now)
+    created_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
--- socialmedia/urls.py
+++ socialmedia/urls.py
@@ -32,5 +32,3 @@ urlpatterns = [
     # path('profile/', user_views.profile, name='profile'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

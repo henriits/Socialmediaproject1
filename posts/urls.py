17f0c86b31563add7ba34ea6c30016133b60d584
katladva <kat.ladva@hotmail.com>
2023-05-27 16:14:47 +0300

    create url not working...

--- posts/urls.py
+++ posts/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 
-from posts.views import AllPostView
+from posts.views import AllPostView, CreatePostView
 from socialmedia.views import HomeView
 
 
@@ -10,5 +10,6 @@ app_name = "posts"
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
-    path("posts/", AllPostView.as_view(), name="allposts")
+    path("posts/", AllPostView.as_view(), name="allposts"),
+    path("create/", CreatePostView.as_view(), name="create"),
 ]

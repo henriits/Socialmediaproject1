226cc4f51b11066f3a7aabbff15345ae514e6b8e
katladva <kat.ladva@hotmail.com>
2023-05-27 15:06:18 +0300

    added view CreatePostView into posts/views.py

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView
-
+from django.views.generic import ListView, CreateView
+from .forms import CreateNewPost
 from .models import Post
 
 
@@ -11,3 +11,10 @@ class AllPostView(ListView):
     template_name = "feed/posts.html"
     success_url = reverse_lazy("posts")
     context_object_name = "posts"
+
+
+class CreatePostView(CreateView):
+    model = Post
+    form_class = CreateNewPost
+    template_name = "posts/create_post.html"
+    success_url = reverse_lazy("create")

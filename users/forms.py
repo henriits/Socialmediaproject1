74cda94116f84405e8ac0855cbad9255e5582324
annabrynkina <an.brynkina@gmail.com>
2023-05-27 20:00:33 +0300

    editing register form fields

--- users/forms.py
+++ users/forms.py
@@ -4,9 +4,23 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField(max_length=254, required=True, help_text='Required. Enter a valid email')
+
+    # def __init__(self, *args, **kwargs):
+    #     super(UserRegisterForm, self).__init__(*args, **kwargs)
+    #     # adding one class for all fields ('class': "form-control py-4")
+    #     # for field_name, field in self.fields.items():
+    #     #     field.widget.attrs['class'] = "form-control py-4"
+
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'First name'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Last name'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'placeholder': 'Enter email'}))
+    # email = forms.EmailField(max_length=254, required=True, help_text='Required. Enter a valid email')
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Repeat password'}))
+
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
 

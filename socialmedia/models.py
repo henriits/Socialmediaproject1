8af29501fb05b54d80c525525e5011d103289d8b
katladva <kat.ladva@hotmail.com>
2023-05-28 11:05:00 +0300

    added models.py in project folder

---
+++ socialmedia/models.py
@@ -0,0 +1,45 @@
+# class User(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE) # if user is deleted = profile deleted as well
+#     image = models.ImageField(default='default_profile_pic.png', upload_to='profile_pics')
+#
+#     def __str__(self):
+#         return f'{self.user.username} Profile'
+#
+# class Post(models.Model):
+#     author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+#     title = models.CharField(max_length=250)
+#     image = models.ImageField(upload_to='img', blank=True, null=True)
+#     text = models.CharField(max_length=250)
+#     created_date = models.DateTimeField(default=timezone.now)
+#
+#     def __str__(self):
+#         return self.title
+#
+# class Comment(models.Model):
+#     comment_id = models.AutoField(primary_key=True)
+#     post_id = models.ForeignKey('posts.Post', on_delete=models.CASCADE)
+#     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#
+#
+# class Like(models.Model):
+#     like_id = models.AutoField(primary_key=True)
+#     post_id = models.ForeignKey('posts.Post', on_delete=models.CASCADE)
+#     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#
+#
+# class Notification(models.Model):
+#     notification_id = models.AutoField(primary_key=True)
+#     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+#     notification_type = models.CharField(max_length=255)
+#     content = models.TextField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     is_read = models.BooleanField(default=False)
+#
+#
+# class Follower(models.Model):
+#     follower_id = models.AutoField(primary_key=True)
+#     user_id = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
+#     follower_user_id = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE)
+#     created_at = models.DateTimeField(auto_now_add=True)

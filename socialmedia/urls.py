ae62fb7a54d8c5a5fc4718944e5dd37e04993bb2
annabrynkina <an.brynkina@gmail.com>
2023-05-29 11:56:45 +0300

    removed logout in urls

--- socialmedia/urls.py
+++ socialmedia/urls.py
@@ -29,7 +29,6 @@ urlpatterns = [
     path("", include(posts_urls, namespace="allposts")),
     path("accounts/", include("django.contrib.auth.urls")),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('register/', user_views.register, name='register'),
     path("", include("posts.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

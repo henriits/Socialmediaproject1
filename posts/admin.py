0c994246dc2a08adcaf332fca64e61ebe7bb53ad
katladva <kat.ladva@hotmail.com>
2023-05-29 22:19:56 +0300

    Added Comments and Like models into posts models.py, added CreateCommentForm into posts forms.py

--- posts/admin.py
+++ posts/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
-
-from posts.models import Post
+from .models import Post, Comments, Like
 
 admin.site.register(Post)
+admin.site.register(Comments)
+admin.site.register(Like)

4bd0a696b7a10d6449d1f456831d9b714eb25ac7
annabrynkina <an.brynkina@gmail.com>
2023-05-29 10:41:36 +0300

    added login method and userlogin form

--- users/forms.py
+++ users/forms.py
@@ -27,10 +27,23 @@
 
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from datetime import datetime
 
 
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        # 'class': "form-control py-4", - moving this to def __init__
+        'placeholder': 'Enter your username'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        # 'class': "form-control py-4",
+        'placeholder': 'Enter your password'
+    }))
+
+    class Meta:
+        model = User
+        fields = ('username', 'password')
 
 class UserRegisterForm(UserCreationForm):
 
--- users/views.py
+++ users/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponseRedirect
+from django.urls import reverse
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserLoginForm
+from django.contrib import auth
 
 # Create your views here.
+
+def login(request):
+    #
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            # if the user is in our system and if he is active, then:
+            if user and user.is_active:
+                # then authorize him
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+        else:
+            print(form.errors)
+    else:
+        form = UserLoginForm()
+    # get request:
+    form = UserLoginForm()
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
 
 def register(request):
     if request.method == 'POST':

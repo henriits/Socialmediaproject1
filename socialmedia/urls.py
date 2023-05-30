abddd7caeeaa44258798f0796ea4eed592f42eb1
annabrynkina <an.brynkina@gmail.com>
2023-05-31 00:13:00 +0300

    changes

--- socialmedia/urls.py
+++ socialmedia/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
     path("", include("posts.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- users/models.py
+++ users/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default_profile_pic.png', upload_to='profile_pics')
@@ -15,3 +14,4 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+

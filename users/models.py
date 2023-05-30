7cee5fb09ae1cea71f0edf10ce99ae6493ce5399
annabrynkina <an.brynkina@gmail.com>
2023-05-30 13:43:39 +0300

    modified profile model

--- users/models.py
+++ users/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
-
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE) # if user is deleted = profile deleted as well
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default_profile_pic.png', upload_to='profile_pics')
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=36)
+    date_of_birth = models.DateField(default='1900-01-01')
+    bio = models.TextField(blank=True)
+    email = models.EmailField()
 
     def __str__(self):
         return f'{self.user.username} Profile'

085e60c9fb3e10c31317bab4759cec2a9e44e3c0
henriits <121551949+henriits@users.noreply.github.com>
2023-05-30 13:17:27 +0300

    added models

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+# notifications/models.py
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Notification(models.Model):
+    notification_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    notification_type = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)

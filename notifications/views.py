c794ab1de0cef6a02f10ab4553373642544370dc
henriits <121551949+henriits@users.noreply.github.com>
2023-05-30 14:06:28 +0300

    notifications view create

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Notification
 
-# Create your views here.
+@login_required
+def notifications(request):
+    user = request.user
+    notifications = Notification.objects.filter(user_id=user, is_read=False)
+
+    if request.method == 'POST':
+        notification_id = request.POST.get('notification_id')
+        notification = Notification.objects.get(notification_id=notification_id)
+        notification.is_read = True
+        notification.save()
+        return redirect('notifications')
+
+    return render(request, 'notifications.html', {'notifications': notifications})

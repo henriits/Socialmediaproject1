79a3a76418c314ca93ebd413132d3e1647a5e008
henriits <121551949+henriits@users.noreply.github.com>
2023-06-05 20:01:41 +0300

    remove notifications from posts and create in notification app

--- notifications/views.py
+++ notifications/views.py
@@ -1,17 +1,33 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-from .models import Notification
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.views import View
 
-@login_required
-def notifications(request):
-    user = request.user
-    notifications = Notification.objects.filter(user_id=user, is_read=False)
+import users
+from notifications.models import Notification
+from posts.models import Post
 
-    if request.method == 'POST':
-        notification_id = request.POST.get('notification_id')
-        notification = Notification.objects.get(notification_id=notification_id)
-        notification.is_read = True
+
+class PostNotification(View):
+    def get(self, request, notification_pk, object_pk, *args, **kwargs):
+        notification = Notification.objects.get(pk=notification_pk)
+        post = Post.objects.get(pk=object_pk)
+        notification.user_has_seen = True
         notification.save()
-        return redirect('notifications')
+        return redirect('post-detail', pk=object_pk)
+class FollowNotification(View):
+    def get(self, request, notification_pk, object_pk, *args, **kwargs):
+        notification = Notification.objects.get(pk=notification_pk)
+        profile = User.objects.get(pk=object_pk)
+        notification.user_has_seen = True
+        notification.save()
+        return redirect('profile', pk=object_pk)
 
-    return render(request, 'notifications.html', {'notifications': notifications})
+class RemoveNotification(View):
+    def delete(self, request, notification_pk, *args, **kwargs):
+        notification = Notification.objects.get(pk=notification_pk)
+
+        notification.user_has_seen = True
+        notification.save()
+
+        return HttpResponse('Success', content_type='text/plain')

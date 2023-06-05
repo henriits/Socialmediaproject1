c382bf626be594a1a8681d6990ec10391235b8fb
henriits <121551949+henriits@users.noreply.github.com>
2023-06-05 21:44:47 +0300

    notifications view

--- notifications/views.py
+++ notifications/views.py
@@ -3,25 +3,40 @@ from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.views import View
 
-import users
+from messaging.models import ThreadModel
 from notifications.models import Notification
 from posts.models import Post
 
 
 class PostNotification(View):
-    def get(self, request, notification_pk, object_pk, *args, **kwargs):
+    def get(self, request, notification_pk, post_pk, *args, **kwargs):
         notification = Notification.objects.get(pk=notification_pk)
-        post = Post.objects.get(pk=object_pk)
+        post = Post.objects.get(pk=post_pk)
+
         notification.user_has_seen = True
         notification.save()
-        return redirect('post-detail', pk=object_pk)
+
+        return redirect('post-detail', pk=post_pk)
+
 class FollowNotification(View):
-    def get(self, request, notification_pk, object_pk, *args, **kwargs):
+    def get(self, request, notification_pk, profile_pk, *args, **kwargs):
         notification = Notification.objects.get(pk=notification_pk)
-        profile = User.objects.get(pk=object_pk)
+        profile = User.objects.get(pk=profile_pk)
+
         notification.user_has_seen = True
         notification.save()
-        return redirect('profile', pk=object_pk)
+
+        return redirect('profile', pk=profile_pk)
+
+class ThreadNotification(View):
+    def get(self, request, notification_pk, object_pk, *args, **kwargs):
+        notification = Notification.objects.get(pk=notification_pk)
+        thread = ThreadModel.objects.get(pk=object_pk)
+
+        notification.user_has_seen = True
+        notification.save()
+
+        return redirect('thread', pk=object_pk)
 
 class RemoveNotification(View):
     def delete(self, request, notification_pk, *args, **kwargs):

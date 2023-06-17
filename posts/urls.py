07be7c2aed8831e13fbe50a70740414d09c8158d
henriits <121551949+henriits@users.noreply.github.com>
2023-06-17 11:56:21 +0300

    removed unused urls

--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.urls import path
 
 from .views import PostDetailView, like_view, post_view, PostDeleteView, PostUpdateView, \
-    comment_like_view, LikeListView, DeleteCommentView, liked_users_view
+    comment_like_view,  DeleteCommentView, liked_users_view
 from socialmedia.views import HomeView
 
 
@@ -18,7 +18,7 @@ urlpatterns = [
     path('<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
     path('<int:pk>/update', PostUpdateView.as_view(), name='post_update'),
     path('like/comment/<int:comment_id>/', comment_like_view, name='comment_like'),
-    path('likes/<int:pk', LikeListView.as_view(), name='likes'),
+    #path('likes/<int:pk', LikeListView.as_view(), name='likes'),
     path('comment/<int:pk>/delete/', DeleteCommentView.as_view(), name='delete_comment'),
     path('liked_users/<int:post_id>', liked_users_view, name='liked_users'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

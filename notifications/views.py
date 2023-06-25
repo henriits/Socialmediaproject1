6b1288b898b6876304174e091fbe66cbb4d6030b
henriits <121551949+henriits@users.noreply.github.com>
2023-06-25 17:04:04 +0300

    changed the notification vies

--- notifications/views.py
+++ notifications/views.py
@@ -1,53 +1,14 @@
-from django.contrib.auth.models import User
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import redirect
-from django.views import View
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Notification
 
-from messaging.models import ThreadModel
-from notifications.models import Notification
-from posts.models import Post
+@login_required
+def show_notifications(request):
+    # Get the notifications for the authenticated user
+    notifications = Notification.objects.filter(to_user=request.user)
 
+    # Mark all notifications as seen
+    notifications.update(user_has_seen=True)
 
-# class PostNotification(View):
-#     def get(self, request, notification_pk, post_pk, *args, **kwargs):
-#         notification = Notification.objects.get(pk=notification_pk)
-#         post = Post.objects.get(pk=post_pk)
-#
-#         notification.user_has_seen = True
-#         notification.save()
-#
-#         return redirect('post-detail', pk=post_pk)
-#
-# class FollowNotification(View):
-#     def get(self, request, notification_pk, profile_pk, *args, **kwargs):
-#         notification = Notification.objects.get(pk=notification_pk)
-#         profile = User.objects.get(pk=profile_pk)
-#
-#         notification.user_has_seen = True
-#         notification.save()
-#
-#         return redirect('profile', pk=profile_pk)
-
-class ThreadNotification(View):
-    def get(self, request, notification_pk, object_pk, *args, **kwargs):
-        notification = Notification.objects.get(pk=notification_pk)
-        thread = ThreadModel.objects.get(pk=object_pk)
-
-        notification.user_has_seen = True
-        notification.save()
-
-        return redirect('thread', pk=object_pk)
-
-class RemoveNotification(View):
-    def delete(self, request, notification_pk, *args, **kwargs):
-        try:
-            notification = Notification.objects.get(pk=notification_pk)
-            notification.delete()  # Delete the notification from the database
-            return HttpResponse('Success', content_type='text/plain')
-        except Notification.DoesNotExist:
-            return HttpResponse('Notification not found', content_type='text/plain', status=404)
-        except Exception as e:
-            return HttpResponse(str(e), content_type='text/plain', status=500)
-
-
-
+    # Render the template with the notifications
+    return render(request, 'show_notifications.html', {'notifications': notifications})

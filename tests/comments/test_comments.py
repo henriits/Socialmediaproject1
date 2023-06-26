da32db75984010d0c0aaa1bb2787358d55e9c0d8
henriits <121551949+henriits@users.noreply.github.com>
2023-06-26 20:30:29 +0300

    add more tests

--- tests/comments/test_comments.py
+++ tests/comments/test_comments.py
@@ -15,3 +15,20 @@ def test_comments():
     assert comment.comment == 'My comment'
     assert comment.user == user
     assert comment.post == post
+
+@pytest.mark.django_db
+def test_add_comment():
+    # Create a user for the post author
+    author = User.objects.create_user(username='testuser', password='testpassword')
+    # Create a post
+    post = Post.objects.create(author=author, title='Test Post', text='This is a test post.')
+    # Create a user for commenting on the post
+    user = User.objects.create_user(username='commenter', password='commenterpassword')
+    comment_text = 'This is a test comment.'
+
+    # Add a comment to the post
+    comment = Comments.objects.create(post=post, user=user, comment=comment_text)
+    assert post.comments.count() == 1
+    assert comment.post == post
+    assert comment.user == user
+    assert comment.comment == comment_text
--- tests/posts/test_posts.py
+++ tests/posts/test_posts.py
@@ -1,6 +1,6 @@
 import pytest
 from django.contrib.auth.models import User
-from posts.models import Post
+from posts.models import Post,Comments
 
 
 @pytest.mark.django_db
@@ -57,3 +57,63 @@ def test_get_post():
     post = Post.objects.create(title='Test Post', text='This is a test post.', author=user)
     retrieved_post = Post.objects.get(title='Test Post')
     assert retrieved_post == post
+
+@pytest.mark.django_db
+def test_post_creation():
+    # Create a user for the post author
+    author = User.objects.create_user(username='testuser', password='testpassword')
+    # Create a post
+    post = Post.objects.create(author=author, title='Test Post', text='Test Post')
+    # Assert post properties
+    assert post.author == author
+    assert post.title == 'Test Post'
+    assert post.text == 'Test Post'
+    assert post.likes.count() == 0
+    assert str(post) == 'Test Post'
+
+
+
+@pytest.mark.django_db
+def test_like_post():
+    # Create a user for the post author
+    author = User.objects.create_user(username='testuser', password='testpassword')
+    # Create a post
+    post = Post.objects.create(author=author, title='Test Post', text='This is a test post.')
+    # Create users for liking the post
+    user1 = User.objects.create_user(username='user1', password='password1')
+    user2 = User.objects.create_user(username='user2', password='password2')
+
+    # User1 likes the post
+    post.likes.add(user1)
+    assert post.likes.count() == 1
+
+    # User2 likes the post
+    post.likes.add(user2)
+    assert post.likes.count() == 2
+
+    # User1 tries to like the post again (duplicate like)
+    post.likes.add(user1)
+    assert post.likes.count() == 2  # Duplicate like should be ignored
+
+
+@pytest.mark.django_db
+def test_unlike_post():
+    # Create a user for the post author
+    author = User.objects.create_user(username='testuser', password='testpassword')
+    # Create a post
+    post = Post.objects.create(author=author, title='Test Post', text='This is a test post.')
+    # Create users for liking the post
+    user1 = User.objects.create_user(username='user1', password='password1')
+    user2 = User.objects.create_user(username='user2', password='password2')
+
+    # User1 and User2 like the post
+    post.likes.add(user1, user2)
+    assert post.likes.count() == 2
+
+    # User1 unlikes the post
+    post.likes.remove(user1)
+    assert post.likes.count() == 1
+
+    # User2 unlikes the post
+    post.likes.remove(user2)
+    assert post.likes.count() == 0
